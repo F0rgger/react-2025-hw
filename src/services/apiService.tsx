c8273110f743ec39252936db5c43adce39e03639
maxforger <maxforge@2gmail.com>
2025-01-10 00:35:24 +0200

    HW2/Task1

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -2,7 +2,7 @@ import {ITodo} from "../models/ITodo.ts";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-export const fetchTodos = (): Promise<ITodo[]> => {
-    return fetch(API_BASE_URL)
+export const fetchTodos = async (): Promise<ITodo[]> => {
+    return await fetch(API_BASE_URL)
         .then(response => response.json());
 };
