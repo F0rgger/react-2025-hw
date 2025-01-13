89411555a5b31df5e44e31dee2c7c49e917d8caa
maxforger <maxforge@2gmail.com>
2025-01-14 00:20:42 +0200

    HW3/Task1

---
+++ src/components/posts-component/PostComponent.tsx
@@ -0,0 +1,7 @@
+import {IPost} from "../../models/IPost.ts";
+interface PostComponentProps {
+    post:IPost;
+}
+export const PostComponent = ({post}:PostComponentProps) => {
+    return <div>{post.title}</div>;
+}
